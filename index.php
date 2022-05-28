1b3f7e771c06b3e5c476318a4fc8853ba8e69905
eduardocDev <eduardoc.dev@gmail.com>
2022-05-28 09:45:07 -0300

    Index criado e editado

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link rel="stylesheet" href="public/css/telaLogin.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  
+    <link rel="stylesheet" href="public/css/telaLogin.css">
 </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class="content-div">
+      <h1>CONTROLE DE ESTOQUE</h1>
+      <a href="out.php"><button type="button" class="btn btn-primary btn-lg">Verificar Estoque</button></a>
+      <a href="in.php"><button type="button" class="btn btn-secondary btn-lg">Adicionar ou Remover</button></a>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </body>
 </html>
