291a8cbedc03808eff6d70a9d709e2e31a05fbc5
Paulo Baltieri <37188572+baltieri7@users.noreply.github.com>
2022-05-27 23:37:32 -0300

    criação tela de login

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>CONTROLE DE ESTOQUE MINIMALISTA</h1>
-    <section>
-        <div>
-            <a href="out.php" class="verify"><b>VERIFICAR ITENS DISPONIVEIS</b></a>
-        </div><br>   
-        <div>
-            <a href="in.php" class="add"><b>ADICIONAR OU REMOVER ITENS</b></a>
-        </div>
-        <?php ?>
-    </section>
+    <h1>tela de login</h1>
 </body>
 </html>
