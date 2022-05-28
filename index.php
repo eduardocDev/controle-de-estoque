865adacf165ba666c368854c86699832f1cf7f62
eduardocDev <eduardoc.dev@gmail.com>
2022-05-27 21:36:25 -0300

    New Updates!

---
+++ index.php
@@ -0,0 +1,21 @@
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>CONTROLE DE ESTOQUE MINIMALISTA</h1>
+    <section>   
+        <div>
+            <a href="in.php" class="add">ADICIONAR OU REMOVER ITENS</a>
+        </div><br>
+        <div>
+            <a href="out.php" class="verify">VERIFICAR ITENS DISPONIVEIS</a>
+        </div>
+    </section>
+</body>
+</html>
